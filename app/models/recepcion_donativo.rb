6710560f2bfc81b54c80e9609d6bae270bf0327d
howardja <howard_ja@hotmail.com>
2015-09-28 12:26:43 -0400

    inicio

--- app/models/recepcion_donativo.rb
+++ app/models/recepcion_donativo.rb
@@ -1,2 +1,9 @@
 class RecepcionDonativo < ActiveRecord::Base
+	validates :nombreDonante, presence: {:message => "- El nombre es un campo obligatorio"}
+	validates_date :fecha, :on => :create, :on_or_before => :today, :on_or_before_message => 'solo se registran donativos a futuro'
+	validates :articulo, presence: {:message => "- El tipo de Articulo es un campo obligatorio"}
+	validates :cantidad, presence: {:message => "- La cantidad es un campo obligatorio"}
+	validates :cantidad, :numericality => {:greater_than => 0, :message => "- La cantidad debe ser mayor a 0"}
+
+
 end
