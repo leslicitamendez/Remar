51a886ee280e85d08a8ec59d1a380341e31428f2
vildozo <vildozo@outlook.com>
2015-09-28 07:32:24 -0700

    correccion error en validacion

--- app/models/interno.rb
+++ app/models/interno.rb
@@ -4,13 +4,10 @@ class Interno < ActiveRecord::Base
 
 	validates :ci, presence: {:message => "- El carnet es un campo obligatorio"}
 	validates :ci, uniqueness: {case_sensitive: false, :message => "- El carnet ya existe"}
-	validates :ci, format: { with: /^\d+#$/, message: "- Solo se aceptan numeros"}
+	validates :ci, format: { with: /!\A[a-zA-Z]+\z/, message: "- Solo se aceptan numeros"}
 	validates :ci, length: { maximum: 7, too_long: "- %{count} caracteres es la longitud maxima permitida" }
 
-	validates :apellido1, presence: {:message => "- El apellido Paterno es un campo obligatorio"}
-	validates :apellido1, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
-	validates :apellido1, length: { maximum: 25, too_long: "- %{count} caracteres es la longitud maxima permitida para apellido Paterno" }
-	
+		
 	validates :apellido2, presence: {:message => "- El apellido Materno es un campo obligatorio"}
 	validates :apellido2, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
 	validates :apellido2, length: { maximum: 25, too_long: "- %{count} caracteres es la longitud maxima permitida para apellido Materno" }
@@ -22,11 +19,11 @@ class Interno < ActiveRecord::Base
 
 	validates :direccion, presence: {:message => "- El nombre es un campo obligatorio"}
 	validates :direccion, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
-	validates :direccion, length: { maximum: 35, too_long: "- %{count} caracteres es la longitud maxima permitida para un nombre" }
+	validates :direccion, length: { maximum: 35, too_long: "- %{count} caracteres es la longitud maxima permitida para un direccion" }
 
 	validates :zona, presence: {:message => "- El nombre es un campo obligatorio"}
 	validates :zona, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
-	validates :zona, length: { maximum: 35, too_long: "- %{count} caracteres es la longitud maxima permitida para un nombre" }
+	validates :zona, length: { maximum: 35, too_long: "- %{count} caracteres es la longitud maxima permitida para una zonanombre" }
 
 	
 end
