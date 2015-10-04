8825009e829280ad9e36f05c639f7fb664f46105
vildozo <vildozo@outlook.com>
2015-10-04 11:35:28 -0400

    arregle vista de internos y cambie tipo de estado de internos

--- app/models/interno.rb
+++ app/models/interno.rb
@@ -10,13 +10,14 @@ class Interno < ActiveRecord::Base
 	validates :ci, uniqueness: { :message => "- El carnet ya existe"}
 	validates :ci, format: { with: /\A[+-]?\d+\z/ , message: " Solo se aceptan numeros"}
 	validates :ci, length: { maximum: 7, too_long: "- %{count} caracteres es la longitud maxima permitida" }
+	validates :ci, length: { minimum: 6, too_long: "- %{count} caracteres es la longitud minima permitida" }
 	
 	validates :apellido1, presence: {:message => "- El apellido Materno es un campo obligatorio"}
-	validates :apellido1, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
+	validates :apellido1, format: { with: /\A[a-zA-Z ]+\z/, message: "- Solo se aceptan letras"}
 	validates :apellido1, length: { maximum: 25, too_long: "- %{count} caracteres es la longitud maxima permitida para apellido Materno" }
 
 	validates :apellido2, presence: {:message => "- El apellido Materno es un campo obligatorio"}
-	validates :apellido2, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
+	validates :apellido2, format: { with: /\A[a-zA-Z ]+\z/, message: "- Solo se aceptan letras"}
 	validates :apellido2, length: { maximum: 25, too_long: "- %{count} caracteres es la longitud maxima permitida para apellido Materno" }
 
 	validates :nombre, presence: {:message => "- El nombre es un campo obligatorio"}
@@ -25,12 +26,22 @@ class Interno < ActiveRecord::Base
 
 
 	validates :direccion, presence: {:message => "- La Direccion es un campo obligatorio"}
-	#validates :direccion, format: { with: /\A[a-zA-Z]+\z/, message: "- Solo se aceptan letras"}
 	validates :direccion, length: { maximum: 35, too_long: "- %{count} caracteres es la longitud maxima permitida para un direccion" }
+	validates :direccion, format: { with: /\A[a-zA-Z0-9 ]+\z/, message: "- Solo se aceptan letras"}
 
 	validates :estadoCivil, presence: {:message => "- El Estado Civil es un campo obligatorio"}
 
 	validates :sexo, presence: {:message => "- El Estado Civil es un campo obligatorio"}
 
+	validates :telefono, length: { maximum: 10, too_long: "- %{count} caracteres es la longitud maxima permitida" }
+	validates :telefono, length: { minimum: 6, too_long: "- %{count} caracteres es la longitud minima permitida" }
+
+	validates :profesion, format: { with: /\A[a-zA-Z ]+\z/, message: "- Solo se aceptan letras"}, allow_blank: true
 	
+	validates :oficio, format: { with: /\A[a-zA-Z ]+\z/, message: "- Solo se aceptan letras"}, allow_blank: true
+
+	validates :estudios, format: { with: /\A[a-zA-Z ]+\z/, message: "- Solo se aceptan letras"}, allow_blank: true
+
+	validates :lugarNacimiento, format: { with: /\A[a-zA-Z ]+\z/, message: "- Solo se aceptan letras"}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930213323) do
+ActiveRecord::Schema.define(version: 20151004152229) do
 
   create_table "avaluo_donativos", force: true do |t|
     t.float    "montoBs"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20150930213323) do
   end
 
   create_table "conyugues", force: true do |t|
-    t.string   "estado"
+    t.boolean  "estado",          limit: 255
     t.string   "nombre"
     t.string   "apellido1"
     t.string   "apellido2"
