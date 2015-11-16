c0f90c422f40658cb328d4a866dc9a3482264788
vildozo <vildozo@outlook.com>
2015-11-16 11:25:48 -0400

    paginacion :)

--- app/controllers/internos_controller.rb
+++ app/controllers/internos_controller.rb
@@ -4,7 +4,8 @@ class InternosController < ApplicationController
   # GET /internos
   # GET /internos.json
   def index
-    @internos = Interno.all
+#    @internos = Interno.all
+     @internos = Interno.paginate(:page => params[:page], :per_page => 5)
     if(params["palabra"]!=nil)
         @internos= Interno.where("(nombre || ' ' || apellido1 || ' ' || apellido2) =?", params[:palabra])    
     end
