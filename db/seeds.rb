6f57531c9c66f902f04e96f0c83ebd90386cba85
vildozo <vildozo@outlook.com>
2015-11-30 10:34:18 -0400

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,20 +22,38 @@ hijos = Hijo.create([{nombre: 'Johan', apellido1: 'Zerna', apellido2: 'Orellana'
 	{nombre: 'Sergio', apellido1: 'Rivera', apellido2: 'Mai', sexo: 'Masculino', ci: 4565981, fechaNacimiento: DateTime.new(1996, 5, 15), lugarNacimiento: 'Cercado Cochabamba', interno_id: '5'}])
 
 
-internos = Interno.create([{nombre: 'Fransisco',  state: 'Activo',apellido1: 'Coca', sexo: 'Masculino', ci: 3938766, estadoCivil: 'Soltero', direccion: 'Chapare', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 2, 28), fechaNacimiento: DateTime.new(1989, 2, 5)},
+internos = Interno.create([
+	{nombre: 'Fransisco',  state: 'Activo',apellido1: 'Coca', sexo: 'Masculino', ci: 3938766, estadoCivil: 'Soltero', direccion: 'Chapare', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 2, 28), fechaNacimiento: DateTime.new(1989, 2, 5)},
 	{nombre: 'Juan', apellido1: 'Mamani', state: 'Activo', apellido2: 'Colque', sexo: 'Masculino', ci: 911348, estadoCivil: 'Divorciado', direccion: 'Capitan Ustariz km 9', lugarNacimiento: 'Nuestra Señora de La Paz La Paz', fechaIngreso: DateTime.new(2015, 1, 5), fechaNacimiento: DateTime.new(1970, 8, 15)},
 	{nombre: 'Rina', apellido1: 'Garcia', apellido2: '',  state: 'Activo',sexo: 'Femenino', ci: 3984622, estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Cercado Cochabamba', fechaIngreso: DateTime.new(2015, 4, 23), fechaNacimiento: DateTime.new(1983, 4, 5)},
 	{nombre: 'Yolanda', apellido1: 'Paz',  state: 'Activo',apellido2: 'Casanovas', sexo: 'Femenino', ci: 959314, estadoCivil: 'Divorciada', direccion: 'Blanco Galindo Km. 3.5', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2014, 11, 24), fechaNacimiento: DateTime.new(1969, 5, 7)},
 	{nombre: 'Julio', apellido1: 'Luna', apellido2: '', state: 'Activo', sexo: 'Masculino', ci: 5757281, estadoCivil: 'Casado', direccion: 'Nuestra SeLa Paz', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2015, 5, 30), fechaNacimiento: DateTime.new(1979, 9, 3)},
 	{nombre: 'Jorge', apellido1: 'Coca', apellido2: 'Quispe', sexo: 'Masculino', state: 'Activo', ci: 853126, estadoCivil: 'Soltero', direccion: 'Sacaba Cochabamba', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Maria', apellido1: 'Nina', apellido2: 'Vargas', sexo: 'Masculino', ci: 999991, state: 'Activo', estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'jose', apellido1: 'Valdivia', apellido2: 'Orellana',  state: 'Activo',sexo: 'Masculino', ci: 7236544, estadoCivil: 'Soltero', direccion: 'Avenida Arenal Santa Cruz', lugarNacimiento: 'Santa Cruz Santa Cruz', fechaIngreso: DateTime.new(2014,3,19), fechaNacimiento: DateTime.new(1990, 3, 15)},	{nombre: 'Maria', apellido1: 'Barriento', apellido2: '', sexo: 'Masculino', ci: 999991, estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Maria', apellido1: 'Barrientos', apellido2: '', sexo: 'Femenino',  state: 'Activo',ci: 999991, estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Sacaba Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Ana', apellido1: 'Coca', apellido2: '', sexo: 'Femenino', ci: 999991,  state: 'Activo',estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Lola', apellido1: 'Valdicia', apellido2: 'Perez', sexo: 'Femenino', ci: 999991,  state: 'Activo',estadoCivil: 'Casado', direccion: 'Puente Antezana', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Fransisca', apellido1: 'Obama', apellido2: '', sexo: 'Femenino', ci: 999991, estadoCivil: 'Sotero', direccion: 'Coronilla', lugarNacimiento: 'Quillacollo Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Erika', apellido1: 'Perez', apellido2: '', sexo: 'Femenino', ci: 999991, state: 'Activo', estadoCivil: 'Soltero', direccion: 'Miamicito', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
-	{nombre: 'Mario', apellido1: 'Coca', apellido2: 'Coca', sexo: 'Femenino', ci: 999991,  state: 'Activo',estadoCivil: 'Soltero', direccion: 'Siles', lugarNacimiento: 'Sacaba Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)}])
+	{nombre: 'Maria', apellido1: 'Nina', apellido2: 'Vargas', sexo: 'Masculino', ci: 4359533, state: 'Activo', estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'jose', apellido1: 'Valdivia', apellido2: 'Orellana',  state: 'Activo',sexo: 'Masculino', ci: 7236544, estadoCivil: 'Soltero', direccion: 'Avenida Arenal Santa Cruz', lugarNacimiento: 'Santa Cruz Santa Cruz', fechaIngreso: DateTime.new(2014,3,19), fechaNacimiento: DateTime.new(1990, 3, 15)},	
+	{nombre: 'Maria', apellido1: 'Barriento', apellido2: '', sexo: 'Masculino', ci: 999991, estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'Maria', apellido1: 'Barrientos', apellido2: '', sexo: 'Femenino',  state: 'Activo',ci: 863512, estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Sacaba Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'Ana', apellido1: 'Coca', apellido2: '', sexo: 'Femenino', ci: 3735682,  state: 'Activo',estadoCivil: 'Casado', direccion: 'La Paz', lugarNacimiento: 'Chapare', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'Lola', apellido1: 'Valdicia', apellido2: 'Perez', sexo: 'Femenino', ci: 211349,  state: 'Activo',estadoCivil: 'Casado', direccion: 'Puente Antezana', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'Fransisca', apellido1: 'Obama', apellido2: '', sexo: 'Femenino', ci: 8127614, estadoCivil: 'Sotero', direccion: 'Coronilla', lugarNacimiento: 'Quillacollo Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'Erika', apellido1: 'Perez', apellido2: '', sexo: 'Femenino', ci: 668319, state: 'Activo', estadoCivil: 'Soltero', direccion: 'Miamicito', lugarNacimiento: 'Chapare Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)},
+	{nombre: 'Mario', apellido1: 'Coca', apellido2: 'Coca', sexo: 'Femenino', ci: 9871377,  state: 'Activo',estadoCivil: 'Soltero', direccion: 'Siles', lugarNacimiento: 'Sacaba Cochabamba', fechaIngreso: DateTime.new(2015, 6, 22), fechaNacimiento: DateTime.new(1975, 6, 22)}
+	])
+
+conyugues = Conyugue.create([
+	   {nombre: 'Marcelino', apellido1: 'Moreno', apellido2: 'Garcia', ci: 4359564 , lugarNacimiento: 'Cercado Cochabamba', fechaNacimiento: DateTime.new(1968, 10, 2), direccion: 'Aroma Nro 1048' , interno_id: 3 },
+	   {nombre: 'Tania', apellido1: 'Colque', apellido2: 'Vaca', ci: 423685, lugarNacimiento: 'San Jose de Chiquitos Santa Cruz', fechaNacimiento: DateTime.new(2015, 4, 13), direccion: 'Ruben Dario Nro 864' , interno_id: 5},
+	   {nombre: 'Dario', apellido1: 'Ruiz', apellido2: '', ci: 3581266, lugarNacimiento: '', fechaNacimiento: DateTime.new(2015, 12, 9), direccion: 'Martin Cardenas 57' , interno_id: 7},
+	   {nombre: 'Gonzalo', apellido1: 'Almanza', apellido2: '', ci: 7869532, lugarNacimiento: '', fechaNacimiento: DateTime.new(2015, 11, 26), direccion: 'Fahrenheit 116' , interno_id: 9},
+	   {nombre: 'Sergio', apellido1: 'Luizaga', apellido2: '', ci: 4896321, lugarNacimiento: 'Quillacollo Cochabamba', fechaNacimiento: DateTime.new(2015, 6, 14), direccion: 'Gabriel Lippmann 43' , interno_id: 10},
+	   {nombre: 'Hugo', apellido1: 'Ortiz', apellido2: 'Sanchez', ci: 4869234, lugarNacimiento: 'Punata Cochabamba', fechaNacimiento: DateTime.new(2015, 1, 10), direccion: 'Zenon Salinas 23' , interno_id: 11}
+
+	])
+
+rehabs = Rehab.create([
+{enfermedades: 'cirrosis hepatica',causaInternacion:'Alcoholismo', tiempoConsumo: 60,centroAnterior:'San Juan de Dios',tiempoEstancia: 4,motivoRetiro:'Concluyo tratamiento',observaciones: 'Problemas nerviosos',interno_id:1}
+
+	])
 
 cities = City.create([{nombre:'Arani', estado: 'Cochabamba'},{nombre:'Arque', estado: 'Cochabamba'},{nombre:'Cercado', estado: 'Cochabamba'},
 	{nombre:'Provincia Ayopaya', estado: 'Cochabamba'},{nombre:'Bolívar', estado: 'Cochabamba'},{nombre:'Capinota', estado: 'Cochabamba'},
